388c84666800d9a4b2510728dcfbb8756892ec85
Eduardo Ezequiel Criscueli <109636703+ezequielcriscueli@users.noreply.github.com>
2024-05-28 02:41:44 -0300

    Update script.js
    Mejoras en el desplazamiento para cada sección correspondiente en dispositivos móviles como en escritorio.

--- script.js
+++ script.js
@@ -34,3 +34,44 @@ function validateForm(event) {
     alert('El formulario se ha enviado correctamente.')
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Selecciona todos los enlaces del menú
+  const menuLinks = document.querySelectorAll('.mobile-menu a, .desktop-menu a');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault(); // Previene el comportamiento predeterminado del enlace
+
+      // Obtiene el ID de la sección objetivo
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      // Calcula la posición de la sección ajustada para no quedar oculta bajo el header
+      const offsetPosition = targetSection.offsetTop - document.querySelector('header').offsetHeight;
+
+      // Desplazamiento suave a la posición calculada
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+
+      // Cierra el menú móvil después de hacer clic si está abierto
+      if (document.querySelector('.mobile-menu').classList.contains('active')) {
+        document.querySelector('.mobile-menu').classList.remove('active');
+      }
+    });
+  });
+});
+
+// Abre el menú al hacer clic en el botón de abrir menú
+document.querySelector('.open-menu-btn').addEventListener('click', function() {
+  var menu = document.querySelector('.mobile-menu');
+  menu.classList.toggle('active');
+});
+
+// Cierra el menú cuando el mouse abandona el área del menú móvil
+document.querySelector('.mobile-menu').addEventListener('mouseleave', function() {
+  var menu = this;
+  menu.classList.remove('active');
+});
